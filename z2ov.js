4760a77eef8ad9d37d68818d0c97aed20981b2c3
Alex <115743030+Vynohradskyi@users.noreply.github.com>
2024-12-20 10:17:06 +0100

    z2ov.js

---
+++ z2ov.js
@@ -0,0 +1,4 @@
+//Nr w dzienniku - 26 ; Kolor tekstu - 3 ; Tekst pogrubiony ; Wielkość liter - 5
+
+document.write("ostatnia modyfikacja strony".fontcolor("gray").bold().fontsize(5)+"<br>");
+document.write(document.lastModified);
